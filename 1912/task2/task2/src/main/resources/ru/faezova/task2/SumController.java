5ac283068fb030fa450d200448765a1ead9ba7ed
Faezova666 <alisafamilia99@gmail.com>
2024-12-28 09:08:26 +0300

    Add files via upload

---
+++ 1912/task2/task2/src/main/resources/ru/faezova/task2/SumController.java
@@ -0,0 +1,34 @@
+package ru.faezova.task1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SumController {
+
+    @FXML
+    private TextField DTextField;
+
+    @FXML
+    private Label DresLabel;
+
+    @FXML
+    private TextField MTextField;
+
+    @FXML
+    private Label MresLabel;
+
+    @FXML
+    private Button resButton;
+
+    @FXML
+    private Label resLabel;
+
+    @FXML
+    void sumButtonOnAction(ActionEvent event) {
+
+    }
+
+}
